c907884acc108090103ca7c3d76c9490913a9fa4
alvinswx <alvinswx@maxis.com.my>
2025-07-12 18:13:04 +0800

    created conn to psql db

--- backend/backend.py
+++ backend/backend.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS # Import CORS to handle cross-origin requests
+import psycopg2
 
 app = Flask(__name__)
 # Enable CORS for all routes, allowing your HTML frontend to make requests
@@ -11,6 +12,13 @@ def home():
     A simple home route to confirm the backend is running.
     """
     return jsonify({"message": "Backend is running! Access /process-search with a POST request."})
+
+def get_db_connection():
+    conn = psycopg2.connect(host='database-1.c7d5tw1qlyo8.ap-southeast-1.rds.amazonaws.com',
+                            database='flask_db',
+                            user='postgres',
+                            password='password')
+    return conn
 
 @app.route('/process-search', methods=['POST'])
 def process_search():
@@ -29,8 +37,18 @@ def process_search():
     original_query = data['search_query']
     processed_query = original_query + "test"
 
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('INSERT INTO query(query, modified)'
+                'VALUES (%s, %s)',
+                (original_query, processed_query))
+    conn.commit()
+    cur.close()
+    conn.close()
+    
     # Return the processed query as a JSON response
     return jsonify({"processed_query": processed_query}), 200
+
 
 if __name__ == '__main__':
     # Run the Flask app on all available interfaces (0.0.0.0) and port 5000
